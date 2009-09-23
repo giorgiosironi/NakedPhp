4fb166d155d6867e379f95ecef2c8ec01fdfe46b
Giorgio Sironi <giorgiosironi@users.sourceforge.net>
2009-09-23 08:32:37 +0000

    added integration testing harness

---
+++ example/application/tests/IndexTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Naked Php is a framework that implements the Naked Objects pattern.
+ * @copyright Copyright (C) 2009  Giorgio Sironi
+ * @license http://www.gnu.org/licenses/lgpl-2.1.txt 
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * @category   Example
+ * @package    Example_Test
+ */
+
+class IndexTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+    public function setUp()
+    {
+        $application = new Zend_Application(
+            'testing', 
+            APPLICATION_PATH . '/configs/application.ini'
+        );
+        $this->bootstrap = array($application, 'bootstrap');
+        parent::setUp();
+    }
+
+    public function testIndex()
+    {
+        $this->dispatch('/naked-php');
+    }
+}
--- example/public/index.php
+++ example/public/index.php
@@ -1,35 +1,5 @@
 <?php
-
-// Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
-
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-
-// Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../../library'),
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
-
-/** Zend_Application */
-require_once 'Zend/Application.php';  
-
-require_once 'NakedPhp/Loader.php';
-$loader = new NakedPhp\Loader();
-
-require_once 'Zend/Loader/Autoloader.php';
-$zfLoader = Zend_Loader_Autoloader::getInstance();
-$zfLoader->pushAutoloader(array($loader, 'autoload'));
-
-$resourceLoader = new Zend_Loader_Autoloader_Resource(array(
-    'basePath'  => APPLICATION_PATH,
-    'namespace' => 'Example'
-));
-$resourceLoader->addResourceType('model', 'models/', 'Model');
+require_once '../application/bootstrap.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
