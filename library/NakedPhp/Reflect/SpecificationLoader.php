31765345f13da4c1466ae9fddcee61086953c1f0
Giorgio Sironi <giorgiosironi@users.sourceforge.net>
2010-02-04 13:20:38 +0000

    added interface

---
+++ library/NakedPhp/Reflect/SpecificationLoader.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Naked Php is a framework that implements the Naked Objects pattern.
+ * @copyright Copyright (C) 2009  Giorgio Sironi
+ * @license http://www.gnu.org/licenses/lgpl-2.1.txt 
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * @category   NakedPhp
+ * @package    NakedPhp_Reflect
+ */
+
+namespace NakedPhp\Reflect;
+use NakedPhp\Metadata\NakedObjectSpecification;
+
+interface SpecificationLoader
+{
+    /**
+     * @param string $className
+     * @return NakedObjectSpecification
+     */
+    public function loadSpecification($className);
+}
