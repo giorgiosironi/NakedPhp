05ca7869f92e4b110f4cdb1d33fe272a2f791e01
Giorgio Sironi <giorgiosironi@users.sourceforge.net>
2010-02-12 08:30:48 +0000

    added SpecificationFactory to encapsulate creation of PhpSpecificationObject (and in general of NakedObjectSpecification concrete classes)

---
+++ library/NakedPhp/Reflect/PhpSpecificationFactory.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Naked Php is a framework that implements the Naked Objects pattern.
+ * @copyright Copyright (C) 2009  Giorgio Sironi
+ * @license http://www.gnu.org/licenses/lgpl-2.1.txt 
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * @category   NakedPhp
+ * @package    NakedPhp_Reflect
+ */
+
+namespace NakedPhp\Reflect;
+use NakedPhp\ProgModel\PhpSpecification;
+
+class PhpSpecificationFactory implements SpecificationFactory
+{
+    protected $_discoverer;
+
+    public function __construct(ClassDiscoverer $discoverer)
+    {
+        $this->_discoverer = $discoverer;
+    }
+
+    /**
+     * @return array    PhpSpecification instances
+     */
+    public function getSpecifications()
+    {
+        $specifications = array();
+        foreach ($this->_discoverer->getList() as $fullName) {
+            $specifications[$fullName] = new PhpSpecification($fullName);
+        }
+        return $specifications;
+    }
+}
---
+++ tests/NakedPhp/Reflect/PhpSpecificationFactoryTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Naked Php is a framework that implements the Naked Objects pattern.
+ * @copyright Copyright (C) 2009  Giorgio Sironi
+ * @license http://www.gnu.org/licenses/lgpl-2.1.txt 
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * @category   NakedPhp
+ * @package    NakedPhp_Reflect
+ */
+
+namespace NakedPhp\Reflect;
+use NakedPhp\ProgModel\PhpSpecification;
+use NakedPhp\Reflect\ClassDiscoverer;
+
+use NakedPhp\MetaModel\AssociationIdentifyingFacetFactory;
+use NakedPhp\Reflect\FacetFactory\AbstractFacetFactory;
+use NakedPhp\Stubs\DummyMethodRemover;
+use NakedPhp\Stubs\FacetHolderStub;
+
+class PhpSpecificationFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreatesPhpSpecificationObjectsGivenSomeClassNames()
+    {
+        $classDiscoverer = new DummyClassDiscoverer();
+        $specFactory = new PhpSpecificationFactory($classDiscoverer);
+        $expected = array(
+            'My_Model_EntityA' => new PhpSpecification('My_Model_EntityA'),
+            'My_Model_EntityB' => new PhpSpecification('My_Model_EntityB')
+        );
+        $this->assertEquals($expected, $specFactory->getSpecifications());
+    }
+}
+
+class DummyClassDiscoverer implements ClassDiscoverer
+{
+    public function getList()
+    {
+        return array(
+            'My_Model_EntityA',
+            'My_Model_EntityB'
+        );
+    }
+}
